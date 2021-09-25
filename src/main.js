f0eaf9e8815bebff311ca945636d44ce7e01b1e7
MatejSmith <mtjkovac@gmail.com>
2021-09-25 13:40:08 +0200

    finish Multiply_by_Length exercise

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 "use strict";
 exports.__esModule = true;
-var Pie = function (total, recipients, each) {
-    return each * recipients <= total;
+var multiplyByLength = function (array) {
+    var x = 0;
+    for (var a = 0; a < array.length; a++) {
+        x = a + 1;
+    }
+    for (var i = 0; i < array.length; i++) {
+        array[i] = array[i] * x;
+    }
+    return array;
 };
-if (!Pie(8, 3, 3)) {
-    console.log("False");
-}
-else {
-    console.log("True");
-}
+console.log(multiplyByLength(new Array(1, 4, 6, 3, 7, 5)));
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 export{};
-let Pie = (total: number, recipients: number, each: number) => {
-    return each * recipients <= total;
+
+let multiplyByLength = (array: Array<number>) => {
+    let x: number = 0;
+    for (let a: number = 0; a < array.length; a++){
+        x = a + 1;
+    }
+    for (let i: number = 0; i < array.length; i++){
+        array[i] = array[i] * x;
+    }
+    return array;
 }
 
-if(!Pie(8, 3, 3)){
-    console.log("False");
-}
-else{
-    console.log("True");
-}
+console.log(multiplyByLength(new Array(1, 4, 6, 3, 7, 5)));
